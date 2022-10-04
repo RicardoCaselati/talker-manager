0f394c6e22ca562ac145dbf63bba0f9077c91016
RicardoCaselati <ricardocaselati@gmail.com>
2022-10-04 15:24:41 -0300

    Req. 01 - ok

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.json());
+
+const pathSpeakers = path.resolve(
+  __dirname, 
+  '..',  
+  'src', 
+  'talker.json',
+);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -12,8 +22,12 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (request, response) => {
+  const speakers = JSON.parse(await fs.readFile(pathSpeakers, 'utf8'));
+  console.log(speakers);
+  response.status(200).json(speakers);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
-
-console.log('começando');
