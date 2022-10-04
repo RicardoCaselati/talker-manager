5320c22f66838e8bc9f3971d20fedf975a0a85b5
RicardoCaselati <ricardocaselati@gmail.com>
2022-10-04 16:09:23 -0300

    Req. 02 - OK

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 const pathSpeakers = path.resolve(
-  __dirname, 
-  '..',  
-  'src', 
+  __dirname,
+  '..',
+  'src',
   'talker.json',
 );
 
@@ -24,8 +24,17 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (request, response) => {
   const speakers = JSON.parse(await fs.readFile(pathSpeakers, 'utf8'));
-  console.log(speakers);
   response.status(200).json(speakers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const speakers = JSON.parse(await fs.readFile(pathSpeakers, 'utf8'));
+  const id = req.params.id;
+  const index = speakers.findIndex((talker) => talker.id === Number(id));
+  if (index < 0) {
+    return res.status(404).json({ "message": "Pessoa palestrante não encontrada" });
+  }
+  res.status(200).json(speakers[index]);
 });
 
 app.listen(PORT, () => {
